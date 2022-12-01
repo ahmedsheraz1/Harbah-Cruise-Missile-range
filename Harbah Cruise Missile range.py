5beae449d824467398e281d3f54a35b0bc9eb4f9
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 12:22:03 +0500

    Create Harbah Cruise Missile range.py

---
+++ Harbah Cruise Missile range.py
@@ -0,0 +1,8 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+sum(i*i for i in range(10))
+285
+xvec = [10, 20, 30]
+yvec = [7, 5, 3]
+sum(x*y for x,y in zip(xvec, yvec))
+260
