5f5a635189d38f1da9aa32c0fd07ed2a937cddd9
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 01:45:09 +0500

    Create Harbah cruise missile.py

---
+++ Harbah cruise missile.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'Harbah cruise missile').hexdigest()
+'90712b0a47f641a1693c50dccb68e17f91dc7fa21385ad9db43e211af4fe15d464f9c56699ce02f2869d0379773a4ee755ee29792d5b5484e4ef12232c12df32'
